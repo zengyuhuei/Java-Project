4fa7c108494f782b6abb54b9b230e379f19fa82f
00557019 <yuniko159753@gmail.com>
2018-12-26 01:35:32 +0800

    reSize pond

--- test/src/java2018/finalProject/PondScreen.java
+++ test/src/java2018/finalProject/PondScreen.java
@@ -61,8 +61,8 @@ public class PondScreen extends JPanel implements ActionListener {
 	
 	public PondScreen(Main mainFrame) {
 		this.mainFrame = mainFrame;
-		this.mainFrame.setSize(800, 800);
-		this.setSize(800, 800);
+		this.mainFrame.setSize(1000, 800);
+		this.setSize(1000, 800);
 		this.setVisible(true);
 
 		this.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -72,12 +72,12 @@ public class PondScreen extends JPanel implements ActionListener {
 		
 		timeLabel = new JLabel("", JLabel.CENTER);
 		timeLabel.setFont(new Font("微軟正黑體 Light", Font.BOLD, 48));
-		timeLabel.setBounds(900, 70, 250, 45);
+		timeLabel.setBounds(700, 70, 250, 45);
 		this.add(timeLabel);
 		
 		startBtn = new JButton("遊戲開始");
 		startBtn.setFont(new Font("微軟正黑體 Light", Font.BOLD, 48));
-		startBtn.setBounds(475, 300, 250, 70);
+		startBtn.setBounds(375, 300, 250, 70);
 		startBtn.addActionListener(this);
 		this.add(startBtn);
 		
@@ -105,7 +105,7 @@ public class PondScreen extends JPanel implements ActionListener {
 		this.add(returnBtn);
 		
 		bgLabel = new JLabel();
-		bgLabel.setSize(1200, 675);
+		bgLabel.setSize(1000, 800);
 		bgLabel.setIcon(iconList.get(0));
 		this.add(bgLabel);
 	}
@@ -137,25 +137,24 @@ public class PondScreen extends JPanel implements ActionListener {
     
     private void buttonSound()
     {
-		 try {
-				File soundFile = new File("..\\sound\\button.wav");
-				AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-				Clip clip = AudioSystem.getClip();
-				clip.open(audioIn);
-				clip.start();
-	            
-	        } catch (UnsupportedAudioFileException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (LineUnavailableException e) {
-	            e.printStackTrace();
-	        }
-    }
+		try {
+			File soundFile = new File("..\\sound\\button.wav");
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			clip.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+}
     
     private void loadBackgroundIcon() {
     	for (int i = 1; i <= 98; ++i) {
-        	iconList.add(imageResize(800, 800, new ImageIcon("../picture/fishing/fishing-" + Integer.toString(i) + ".png")));
+        	iconList.add(imageResize(1000, 800, new ImageIcon("../picture/fishing/fishing-" + Integer.toString(i) + ".png")));
     	}
     }
 
@@ -163,6 +162,7 @@ public class PondScreen extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource() == returnBtn) {
+			this.mainFrame.setSize(1200, 675);
 			this.mainFrame.changeToMainScreen();
 		}
 		else if (e.getSource() == startBtn) {
